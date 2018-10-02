f207eb84beb85320d1394402cbd5f92329e4cfee
Jeff <jeffrey.sabri@hotmail.com>
2018-10-02 16:54:47 +1000

    got working what type of encryption the server has chosen for handshake

--- main.py
+++ main.py
@@ -7,19 +7,20 @@ import csv
 import numpy
 import pandas
 import operator
+
+
 # import RandomnessTester
 
 def entropy(string):
-        "Calculates the Shannon entropy of a string"
+    "Calculates the Shannon entropy of a string"
 
-        # get probability of chars in string
-        prob = [ float(string.count(c)) / len(string) for c in dict.fromkeys(list(string)) ]
+    # get probability of chars in string
+    prob = [float(string.count(c)) / len(string) for c in dict.fromkeys(list(string))]
 
-        # calculate the entropy
-        entropy = - sum([ p * math.log(p) / math.log(2.0) for p in prob ])
+    # calculate the entropy
+    entropy = - sum([p * math.log(p) / math.log(2.0) for p in prob])
 
-        return entropy
-
+    return entropy
 
 
 def byte_entropy(flow):
@@ -34,14 +35,15 @@ def byte_entropy(flow):
         return
     return flow_info
 
-def shannon (data):
+
+def shannon(data):
     # Whithin the for statement, we determine the frequency of each byte
     # in the dataset and if this frequency is not null we use it for the
     # entropy calculation
 
     dataSize = len(data)
     ent = 0.0
-    freq={}
+    freq = {}
     # for c in data:
     for c in map(operator.add, data[::2], data[1::2]):
         if freq.get(c):
@@ -49,13 +51,13 @@ def shannon (data):
         else:
             freq[c] = 1
 
-   # a byte can take 256 values from 0 to 255. Here we are looping 256 times
-   # to determine if each possible value of a byte is in the dataset
+    # a byte can take 256 values from 0 to 255. Here we are looping 256 times
+    # to determine if each possible value of a byte is in the dataset
     for key in freq.keys():
-        f = float(freq[key])/dataSize+0.0
-        if f > 0: # to avoid an error for log(0)
+        f = float(freq[key]) / dataSize + 0.0
+        if f > 0:  # to avoid an error for log(0)
             ent = ent + f * log(f, 2)
-    return -ent  if ent else 0.00
+    return -ent if ent else 0.00
 
 
 def entropy_ideal(length):
@@ -69,77 +71,92 @@ def entropy_ideal(length):
 # ref.: http://lorenzoriano.wordpress.com/tag/python/
 def kolmogorov(data):
     if data == None or data == '':
-
         return 0
-    my_str_as_bytes = str.encode(data)
+    my_str_as_bytes = bytearray.fromhex(data)
     l = float(len(data))
     compr = zlib.compress(my_str_as_bytes)
-    c = float(len(compr))/l
+    c = float(len(compr)) / l
     if c > 1:
         return 1.0
     else:
         return c
 
+
 if __name__ == "__main__":
 
-    flow=[]
+    flow = []
     byte_entropy_per_flow = []
     with open('json') as f:
         data = json.load(f);
 
-    total_flows= len(data)
+    total_flows = len(data)
     # pprint(data)
-    tcp_data_ex=[]
-        # print(data['layers']['frame']['tcp.payload'])
-    tcp_data_ex2=[]
+    tcp_data_ex = []
+    # print(data['layers']['frame']['tcp.payload'])
+    tcp_data_ex2 = []
 
-
+    # this is for cipher suite the data is saved as an integer, got to convert this back to hex and check with file if recommended
+    # the code for it is packet[u'_source'][u'layers'][u'ssl'][u'ssl.record'][ssl.handshake.ciphersuite]
 
     for packet in data:
-         try:
+        try:
 
-                 print(packet[u'_source'][u'layers'][u'frame'][u'frame.number'][0:])
-                 print(type(packet[u'_source'][u'layers'][u'tcp'][u'tcp.payload']))
-                 # print((packet[u'_source'][u'layers'][u'ssl'][u'ssl.record']))
+            # print(packet[u'_source'][u'layers'][u'frame'][u'frame.number'][0:])
+            # print(type(packet[u'_source'][u'layers'][u'tcp'][u'tcp.payload']))
+            # print((packet[u'_source'][u'layers'][u'ssl'][u'ssl.record']))
 
-                 # tcp_data_ex.append(packet[u'_source'][u'layers'][u'tcp'][u'tcp.payload'])
+            # tcp_data_ex.append(packet[u'_source'][u'layers'][u'tcp'][u'tcp.payload'])
 
-                 # print(packet[u'_source'][u'layers'][u'tcp'][u'tcp.payload'])
-                 # print(packet[u'_source'][u'layers'][u'ssl'][u'ssl.record'][u'ssl.app_data'])
-                 print("this is the length", len(packet[u'_source'][u'layers'][u'ssl']))
+            # print(packet[u'_source'][u'layers'][u'tcp'][u'tcp.payload'])
+            # print(packet[u'_source'][u'layers'][u'ssl'][u'ssl.record'][u'ssl.app_data'])
+            # print("this is the length", len(packet[u'_source'][u'layers'][u'ssl']))
 
-                 if(len(packet[u'_source'][u'layers'][u'ssl'])== 1):
-                    print("this is the length", len(packet[u'_source'][u'layers'][u'ssl']))
-                    print(packet[u'_source'][u'layers'][u'ssl'][u'ssl.record'][u'ssl.app_data'])
-                    print(type(packet[u'_source'][u'layers'][u'ssl'][u'ssl.record'][u'ssl.app_data']))
-                    tcp_data_ex.append(packet[u'_source'][u'layers'][u'ssl'][u'ssl.record'][u'ssl.app_data'])
+            if (len(packet[u'_source'][u'layers'][u'ssl']) == 1):
+                if ((
+                int(packet[u'_source'][u'layers'][u'ssl'][u'ssl.record'][u'ssl.record.content_type'])) == 22 and int(
+                        packet[u'_source'][u'layers'][u'ssl'][u'ssl.record'][u'ssl.record.length']) > 45):
+                    # print ("length of ssl", (packet[u'_source'][u'layers'][u'ssl']))
+                    print(int(
+                        packet[u'_source'][u'layers'][u'ssl'][u'ssl.record'][u'ssl.handshake'][u'ssl.handshake.type']))
+                    print(type(int(
+                        packet[u'_source'][u'layers'][u'ssl'][u'ssl.record'][u'ssl.handshake'][u'ssl.handshake.type'])))
+
+                    if (int(packet[u'_source'][u'layers'][u'ssl'][u'ssl.record'][u'ssl.handshake'][
+                                u'ssl.handshake.type']) == 2):
+                        print(("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD",hex(int(packet[u'_source'][u'layers'][u'ssl'][u'ssl.record'][u'ssl.handshake'][u'ssl.handshake.ciphersuite']))))
+
+                # print("this is the length", len(packet[u'_source'][u'layers'][u'ssl']))
+                # print(packet[u'_source'][u'layers'][u'ssl'][u'ssl.record'][u'ssl.app_data'])
+                # print(type(packet[u'_source'][u'layers'][u'ssl'][u'ssl.record'][u'ssl.app_data']))
+                tcp_data_ex.append(packet[u'_source'][u'layers'][u'ssl'][u'ssl.record'][u'ssl.app_data'])
+                # print((packet[u'_source'][u'layers'][u'ssl']))
+
+                # if (len(packet[u'_source'][u'layers'][u'ssl'][u'ssl.record'])==1):
+                # print("the code for it is", packet[u'_source'][u'layers'][u'ssl'][u'ssl.record'][u'ssl.record.content_type'])
 
 
-         except KeyError:
-                continue
+        except KeyError:
+            continue
 
     # print(tcp_data_ex)
 
-
-
     # print ("abcdsadsa", tcp_data_ex )
-    print ("length of ssl strip" ,len(tcp_data_ex))
-    x=0
-
+    print("length of ssl strip", len(tcp_data_ex))
+    x = 0
 
     for elem in tcp_data_ex:
-        if x==1:
+        if x == 1:
             # g_data= g_data + (":").encode('latin-1')+elem.encode('latin-1')
             # print(elem.encode('latin1'))
-            g_data= g_data + elem
+            g_data = g_data + elem
 
             # g_data.extend(elem)
         else:
-            x=1
+            x = 1
             # g_data= elem.encode('latin-1')
             g_data = elem
             # g_data=bytearray()
-            print (type(g_data),"this is it")
+            print(type(g_data), "this is it")
             print(elem)
 
     f.close()
@@ -149,23 +166,19 @@ if __name__ == "__main__":
     print(len(newstr))
     # print(newst(r)
     # newstr=("16030100")
-    bin_data=(bin(int(newstr, 16))[2:]).zfill(len(newstr*4))
-
-
+    bin_data = (bin(int(newstr, 16))[2:]).zfill(len(newstr * 4))
 
     print("length of binary data", len(bin_data))
 
     # print("correct data",bin_data)
 
-
-
-    value= shannon(newstr)
-    value2= entropy_ideal(len(bin_data))
-    value3=kolmogorov(newstr)
-    print (value)
+    value = shannon(newstr)
+    value2 = entropy_ideal(len(bin_data))
+    value3 = kolmogorov(newstr)
+    print(value)
     print("second value of shannon", entropy(newstr))
-    print ("ideal shannon entropy",value2)
-    print ("kolmogorov entropy calculation", value3)
+    print("ideal shannon entropy", value2)
+    print("kolmogorov entropy calculation", value3)
     # print("this is prior to conversion", newstr)
 
     """
